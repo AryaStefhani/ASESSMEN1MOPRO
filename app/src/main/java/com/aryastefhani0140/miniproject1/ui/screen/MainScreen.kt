4050a9e542935a85dce980428c809679fa820b8f
Arya StefhaniSinaga <hanpanka@gmail.com>
2025-04-02 17:14:57 +0700

    Menambahkan input jumlah uang dari pengguna

--- app/src/main/java/com/aryastefhani0140/miniproject1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aryastefhani0140/miniproject1/ui/screen/MainScreen.kt
@@ -1,17 +1,30 @@
 package com.aryastefhani0140.miniproject1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aryastefhani0140.miniproject1.R
 import com.aryastefhani0140.miniproject1.ui.theme.Miniproject1Theme
 
@@ -37,10 +50,24 @@ fun MainScreen() {
 }
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    var amount by remember { mutableStateOf("") }
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+   ){
+        Text(text = stringResource(R.string.app_description))
+
+        TextField(
+            value = amount,
+            onValueChange = { amount = it },
+            label = { Text(stringResource(R.string.jumlah_uang)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
